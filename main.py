26894e6fcdfdff16fbb3daa517649df74ed16f76
Aiden Grishaber <agrishaber24@biomedscienceacademy.org>
2021-01-21 18:13:03 +0000

    I added to the main function.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ Period/Core:
 
 import os
 import importlib.util
-
 import random
 
 def run_tests():
@@ -18,14 +17,37 @@ def run_tests():
   if importlib.util.find_spec('pytest') is None: # Check if pytest is installed
     os.system('python3 -m pip install -q pytest')
 
-  command = "python3 -m pytest --tb=line -v -s tests.py"
+  command = "python3 -m pytest --tb=line -v tests.py"
   print(command)
   os.system(command)
 
 def main():
-  """This function contains all code for the chatbot."""
-  print("Hello!")
+  name = input("What is your name? ")
+  print("Hello there " + name + " my name is chatbot!")
 
+  color = input("What is your favorite color? ")
+  if color == "Blue":
+    print(" You have the most popular favorote color!")
+  else:
+    print("Blue is the most popular favorite color, but " + color + " is my favorite too.")
+  emotion = input("How are you feeling today " + name +"?")
+
+
+  name = input("What is your name? ")
+  print("Hello there " + name + " my name is chatbot!")
+
+
+
+
+
+  color = input("What is your favorite color? ")
+  if color == "Blue":
+    print(" You have the most popular favorote color!")
+  else:
+    print("Blue is the most popular favorite color, but " + color + " is my favorite too.")
+
+
+  emotion = input("How are you doing today " + name +"?")
 
 if __name__ == "__main__":
   main()
